af5d832d00303afeb51cfeae759c90d61e751a81
Martín Vásquez <pedromvasquezb@gmail.com>
2017-08-19 02:02:23 -0500

    Add DetailActivity 'Share' menu item

--- app/src/main/java/com/example/android/sunshine/DetailActivity.kt
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.kt
@@ -1,18 +1,51 @@
 package com.example.android.sunshine
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ShareCompat
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 
 class DetailActivity : AppCompatActivity() {
 
+    companion object {
+        val SHARE_HASHTAG = "#SunshineApp"
+    }
+
     private lateinit var mWeatherDetail: TextView
+    private lateinit var mForecast: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+        mForecast = intent.getStringExtra("weatherData") ?: ""
+
         mWeatherDetail = findViewById(R.id.weather_detail) as TextView
-        mWeatherDetail.text = intent.getStringExtra("weatherData")
+        mWeatherDetail.text = mForecast
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.btn_action_refresh -> {
+                shareForecast()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun shareForecast() {
+        val shareIntent = ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText("$mForecast $SHARE_HASHTAG")
+                .startChooser()
     }
 }
