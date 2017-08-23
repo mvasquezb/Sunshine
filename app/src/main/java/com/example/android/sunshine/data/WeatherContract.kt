b704717ca9a367f4526f25f2f2ae6d5471e2496c
Martín Vásquez <pedromvasquezb@gmail.com>
2017-08-23 01:03:27 -0500

    Add CONTENT_URI and util methods to WeatherContract

--- app/src/main/java/com/example/android/sunshine/data/WeatherContract.kt
+++ app/src/main/java/com/example/android/sunshine/data/WeatherContract.kt
@@ -1,12 +1,20 @@
 package com.example.android.sunshine.data
 
+import android.net.Uri
 import android.provider.BaseColumns
+import com.example.android.sunshine.utilities.SunshineDateUtils
 
 /**
  * Defines table and column names for the weather database. This class is not necessary, but keeps
  * the code organized.
  */
 class WeatherContract {
+
+    companion object {
+        val CONTENT_AUTHORITY = "com.example.android.sunshine"
+        val BASE_CONTENT_URI = "content://$CONTENT_AUTHORITY"
+    }
+
     class WeatherEntry : BaseColumns {
         companion object {
             val TABLE_NAME = "weather"
@@ -18,6 +26,20 @@ class WeatherContract {
             val COLUMN_PRESSURE = "pressure"
             val COLUMN_WIND_SPEED = "wind"
             val COLUMN_DEGREES = "degrees"
+
+            val PATH_WEATHER = "weather"
+            val CONTENT_URI = Uri.parse("$BASE_CONTENT_URI/$PATH_WEATHER")
+
+            @JvmStatic
+            fun buildWeatherUriWithDate(date: Long): Uri {
+                return CONTENT_URI.buildUpon().appendPath(date.toString()).build()
+            }
+
+            @JvmStatic
+            fun getSelectFromTodayOnwards(): String {
+                val normalizedNow = SunshineDateUtils.normalizeDate(System.currentTimeMillis())
+                return "${WeatherEntry.COLUMN_DATE} >= $normalizedNow"
+            }
         }
     }
 }
