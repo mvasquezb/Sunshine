b80540fde33d04868f69cbd24e1777ddddac648c
Martín Vásquez <pedromvasquezb@gmail.com>
2017-08-25 13:24:49 -0500

    Remove name parameter from SunshineSyncService constructor

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncService.kt
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncService.kt
@@ -6,7 +6,7 @@ import android.content.Intent
 /**
  * Created by pmvb on 17-08-25.
  */
-class SunshineSyncService(name: String) : IntentService(name) {
+class SunshineSyncService : IntentService(SunshineSyncService::class.java.simpleName) {
     override fun onHandleIntent(intent: Intent?) {
         SunshineSyncTask.syncWeather(this)
     }
