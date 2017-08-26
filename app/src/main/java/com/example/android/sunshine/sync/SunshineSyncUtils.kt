c1c5c2111f428c4831fcfd8215a041e3df770f3b
Martín Vásquez <pedromvasquezb@gmail.com>
2017-08-26 02:54:05 -0500

    Add firebase job scheduling

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.kt
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.kt
@@ -6,19 +6,48 @@ import android.database.Cursor
 import android.os.AsyncTask
 import android.provider.BaseColumns
 import com.example.android.sunshine.data.WeatherContract
+import com.firebase.jobdispatcher.*
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by pmvb on 17-08-25.
  */
 object SunshineSyncUtils {
 
+    private val SYNC_INTERVAL_HOURS = 3
+    private val SYNC_INTERVAL_SECONDS = TimeUnit.HOURS.toSeconds(SYNC_INTERVAL_HOURS.toLong()).toInt()
+    private val SYNC_FLEXTIME_SECONDS = SYNC_INTERVAL_SECONDS / 3
+
+    private val SUNSHINE_SYNC_TAG = "sunshine-sync"
+
     private var sInitialized = false
+
+    fun scheduleDispatcherSync(context: Context) {
+        val driver = GooglePlayDriver(context)
+        val jobDispatcher = FirebaseJobDispatcher(driver)
+        val syncJob = jobDispatcher.newJobBuilder()
+                .setService(SunshineFirebaseJobService::class.java)
+                .setTag(SUNSHINE_SYNC_TAG)
+                .setConstraints(Constraint.ON_ANY_NETWORK)
+                .setLifetime(Lifetime.FOREVER)
+                .setRecurring(true)
+                .setTrigger(Trigger.executionWindow(
+                        SYNC_INTERVAL_SECONDS,
+                        SYNC_INTERVAL_HOURS + SYNC_FLEXTIME_SECONDS
+                ))
+                .setReplaceCurrent(true)
+                .build()
+        jobDispatcher.schedule(syncJob)
+    }
 
     @Synchronized
     fun initialize(context: Context) {
         if (sInitialized) {
             return
         }
+        sInitialized = true
+
+        scheduleDispatcherSync(context)
 
         val queryTask = object : AsyncTask<Unit, Unit, Unit>() {
             override fun doInBackground(vararg params: Unit?) {
@@ -40,8 +69,6 @@ object SunshineSyncUtils {
             }
         }
         queryTask.execute()
-
-        sInitialized = true
     }
 
     fun startImmediateSync(context: Context) {
