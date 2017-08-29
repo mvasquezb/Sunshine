fb4c0729f901c8c7a64be7ea20c7cc22f534ce3e
Martín Vásquez <pedromvasquezb@gmail.com>
2017-08-28 22:58:53 -0500

    Modify list item layout

--- app/src/main/java/com/example/android/sunshine/ForecastAdapter.kt
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.kt
@@ -6,8 +6,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
-import com.example.android.sunshine.data.WeatherContract
 import com.example.android.sunshine.utilities.SunshineDateUtils
 import com.example.android.sunshine.utilities.SunshineWeatherUtils
 
@@ -45,23 +45,40 @@ class ForecastAdapter(
         if (!cursor.moveToPosition(position)) {
             return
         }
+        /**
+         * Weather date
+         */
         val date = cursor.getLong(MainActivity.INDEX_WEATHER_DATE)
         val dateStr = SunshineDateUtils.getFriendlyDateString(
                 mContext,
                 date
         )
-        val descStr = SunshineWeatherUtils.getStringForWeatherCondition(
-                mContext,
-                cursor.getInt(MainActivity.INDEX_WEATHER_WEATHER_ID)
-        )
-        val maxMinTempStr = SunshineWeatherUtils.formatHighLows(
-                mContext,
-                cursor.getDouble(MainActivity.INDEX_WEATHER_MAX_TEMP),
-                cursor.getDouble(MainActivity.INDEX_WEATHER_MIN_TEMP)
-        )
+        holder.mDateText.text = dateStr
 
-        var weatherSummary = "$dateStr - $descStr - $maxMinTempStr"
-        holder.mWeatherText.text = weatherSummary
+        /**
+         * Weather condition icon
+         */
+        val weatherConditionId = cursor.getInt(MainActivity.INDEX_WEATHER_WEATHER_ID)
+        holder.mWeatherIcon.setImageResource(
+                SunshineWeatherUtils.getSmallArtResourceIdForWeatherCondition(weatherConditionId)
+        )
+        /**
+         * Weather condition text
+         */
+        val conditionStr = SunshineWeatherUtils.getStringForWeatherCondition(
+                mContext,
+                weatherConditionId
+        )
+        holder.mConditionText.text = conditionStr
+        /**
+         * High and low temperature
+         */
+        val highTemp = cursor.getDouble(MainActivity.INDEX_WEATHER_MAX_TEMP)
+        val lowTemp = cursor.getDouble(MainActivity.INDEX_WEATHER_MIN_TEMP)
+
+        holder.mHighTempText.text = SunshineWeatherUtils.formatTemperature(mContext, highTemp)
+        holder.mLowTempText.text = SunshineWeatherUtils.formatTemperature(mContext, lowTemp)
+
         holder.mForecastDate = date
     }
 
@@ -70,11 +87,6 @@ class ForecastAdapter(
             return 0
         }
         return mCursor!!.count
-    }
-
-    fun setWeatherData(weatherData: Array<String>) {
-//        mWeatherData = weatherData
-        notifyDataSetChanged()
     }
 
     fun swapCursor(cursor: Cursor?) {
@@ -86,7 +98,12 @@ class ForecastAdapter(
             RecyclerView.ViewHolder(itemView),
             View.OnClickListener {
 
-        val mWeatherText: TextView = itemView.findViewById(R.id.weather_data) as TextView
+        val mDateText = itemView.findViewById(R.id.dateText) as TextView
+        val mConditionText = itemView.findViewById(R.id.weatherDescription) as TextView
+        val mHighTempText = itemView.findViewById(R.id.highTempText) as TextView
+        val mLowTempText = itemView.findViewById(R.id.lowTempText) as TextView
+        val mWeatherIcon = itemView.findViewById(R.id.weatherIcon) as ImageView
+
         var mForecastDate: Long = System.currentTimeMillis()
 
         init {
